9495455c072ba26ac0d6614a36b2a1c853be96cd
TAKAHASHI Shuuji <shuuji3@gmail.com>
2019-08-12 17:18:07 +0900

    Implement single byte read() method

--- src/main/scala/org/apache/hadoop/fs/ceph/CephFSInputStream.scala
+++ src/main/scala/org/apache/hadoop/fs/ceph/CephFSInputStream.scala
@@ -1,13 +1,18 @@
 package org.apache.hadoop.fs.ceph
 
 import java.io.{EOFException, IOException}
-import java.nio.channels.SeekableByteChannel
+import java.nio.ByteBuffer
 
 import com.ceph.rados.IoCTX
 import org.apache.hadoop.fs.FSInputStream
 
 class CephFSInputStream(ioCtx: IoCTX, objectName: String, bufferSize: Int) extends FSInputStream {
-  val channel: SeekableByteChannel = new CephReadChannel(ioCtx, objectName, bufferSize)
+
+  val channel = new CephReadChannel(ioCtx, objectName, bufferSize)
+
+  // used for read() to read 1 byte
+  val oneByteBuffer = new Array[Byte](1)
+
   /**
    * Seeks a different copy of the data.  Returns true if
    * found a new source, false otherwise.
@@ -16,23 +21,6 @@ class CephFSInputStream(ioCtx: IoCTX, objectName: String, bufferSize: Int) exten
    */
   @throws[IOException]
   override def seekToNewSource(targetPos: Long) = false
-
-  /**
-   * Reads the next byte of data from the input stream. The value byte is
-   * returned as an <code>int</code> in the range <code>0</code> to
-   * <code>255</code>. If no byte is available because the end of the stream
-   * has been reached, the value <code>-1</code> is returned. This method
-   * blocks until input data is available, the end of the stream is detected,
-   * or an exception is thrown.
-   *
-   * <p> A subclass must provide an implementation of this method.
-   *
-   * @return the next byte of data, or <code>-1</code> if the end of the
-   *         stream is reached.
-   * @throws IOException if an I/O error occurs.
-   */
-  @throws[IOException]
-  override def read = 0
 
   @throws[IOException]
   override def read(position: Long, buffer: Array[Byte], offset: Int, length: Int): Int = synchronized {
@@ -59,15 +47,40 @@ class CephFSInputStream(ioCtx: IoCTX, objectName: String, bufferSize: Int) exten
    * The next read() will be from that location.  Can't
    * seek past the end of the file.
    *
-   * @param pos
+   * @param pos position to seek
    */
   @throws[IOException]
   override def seek(pos: Long): Unit = {
+    try {
+      channel.position(pos)
+    } catch {
+      case e: IllegalArgumentException => throw new IOException(e)
+    }
   }
 
   /**
    * Return the current offset from the start of the file
    */
   @throws[IOException]
-  override def getPos = 0
+  override def getPos: Long = channel.position()
+
+  /**
+   * Reads the next byte of data from the input stream. The value byte is
+   * returned as an <code>int</code> in the range <code>0</code> to
+   * <code>255</code>. If no byte is available because the end of the stream
+   * has been reached, the value <code>-1</code> is returned. This method
+   * blocks until input data is available, the end of the stream is detected,
+   * or an exception is thrown.
+   *
+   * <p> A subclass must provide an implementation of this method.
+   *
+   * @return the next byte of data, or <code>-1</code> if the end of the
+   *         stream is reached.
+   * @throws IOException if an I/O error occurs.
+   */
+  @throws[IOException]
+  override def read: Int = {
+    val numRead = channel.read(ByteBuffer.wrap(oneByteBuffer))
+    numRead
+  }
 }
